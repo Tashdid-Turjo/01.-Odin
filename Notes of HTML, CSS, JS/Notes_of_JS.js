36529309ad3924871c5280fa6a9316c4587e61da
Tashdid-Turjo <tashdid.turjo@gmail.com>
2025-11-20 17:52:48 +0600

    js note updated

--- Notes of HTML, CSS, JS/Notes_of_JS.js
+++ Notes of HTML, CSS, JS/Notes_of_JS.js
@@ -1,7 +1,7 @@
 // ✅JS:
 /* 
 -> Keywords:
-            -> Variables(using let, const), & Operators(number, string, logical, mathematical)
+            -> Variables(using let, const), & Operators(number, string, logical, mathematical) (Terms- unary, binary, operand, prefix, postfix),
             -> Run type -> via browser,
                         -> outside browser environment (that means it will let the browser execute the code, even if it comes from a a file we created.)
 
@@ -21,7 +21,7 @@
     <title>Document</title>
 </head>
 <body>
-    {/* i. Inside HTML, write JS:*/} 
+    {/* i. Inside HTML, write JS: */} 
     <script>
         console.log("Hello, World!")
     </script>
@@ -37,7 +37,7 @@
 
 // !! 2. let ->   we can re-assign //
 // !!    const -> we can't re-assign, thus, it will throw error if we try. //
-// !!    var -> it can be used as let, but now-a-days, this ain't used much. //
+// !!    var ->   it can be used as let, but now-a-days, this ain't used much. //
 
 // !! -----------------------Ex01 ------------------------------------------------*/
 {/* HTML */}
@@ -86,3 +86,35 @@ function updateBtn() {
 
 
 {/*--- Start from the Odin's "variables and operators" and search this- "read through". ---*/}
+
+
+
+// !! ----------------------- Ex03 ------------------------------------------------*/
+// !! prefix, postfix //
+let i = 5;
+let j = 5;
+
+// Prefix Example (++i):
+// 1. i becomes 6
+// 2. The NEW value (6) is assigned to a
+let a = ++i; 
+
+// Postfix Example (j++):
+// 1. The OLD value (5) is assigned to b
+// 2. j becomes 6
+let b = j++; 
+
+console.log("i:", i); // Output: i: 6
+console.log("a:", a); // Output: a: 6 (It took the new value of i)
+console.log("j:", j); // Output: j: 6
+console.log("b:", b); // Output: b: 5 (It took the old value of j)
+
+
+
+// !! ----------------------- Ex04 ------------------------------------------------*/
+let counter = 0;
+alert( ++counter ); // 1
+
+// !! ----------------------- Ex05 ------------------------------------------------*/
+let counter = 0;
+alert( counter++ ); // 0
