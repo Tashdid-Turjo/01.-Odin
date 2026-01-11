215e16b6aced8ea952740dd524f7c41e7b8a9db1
Tashdid-Turjo <tashdid.turjo@gmail.com>
2026-01-11 10:20:57 +0600

    React

--- Notes of HTML, CSS, JS/react-basics.jsx
+++ Notes of HTML, CSS, JS/react-basics.jsx
@@ -1,2 +1,13 @@
 // From YT: SuperSimpleDev
+// React -> External Library(other's code that we can use for our website.)
 
+
+
+// !! Inside HTML Code (Loading JS code from another website):
+<body>
+    {/* <!-- !! for loading js code from another website. --> */}
+    <script src="https://unpkg.com/supersimpledev@3.2.1/external-library.js"></script>
+</body>
+
+// N.B: This is called External library.
+// N.B: External Library -> a code that's outside of our computer / someone else's code that's written in a different website.
