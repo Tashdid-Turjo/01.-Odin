5b615228b0d779840433e9a1c080731061416d8d
Tashdid-Turjo <tashdid.turjo@gmail.com>
2026-06-01 15:36:20 +0600

    SQL

---
+++ BackEnd(Database, Nodejs)/PostGreSQL_Database.js
@@ -0,0 +1,11 @@
+// PostGreSQL -> to organize and capture large amounts of data, typically by inputting, storing, retrieving and managing the information. This course will focus on relational databases.
+
+// SQL -> Structured Query Language
+
+//!! Basics of SQL:
+// create table, insert data, query data[using SELECT], aggregate data[SUM(), AVG(), COUNT(), MAX(), MIN()]
+
+
+
+// !! Intermediate of SQL:
+// update data, delete data, combine data -> across tables
